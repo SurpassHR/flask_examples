f8dcea1fc424676c02ba4212a991d697fa574a09
surpasshr <hu.rui0530@gmail.com>
2023-10-25 21:24:08 +0800

    ssh打桩

--- flask_test.py
+++ flask_test.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from stub_ssh_response import StubBoardResponse
 import json
 
 app = Flask(__name__)
---
+++ stub_ssh_response.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+hostIpPath = "hostip.txt"
+CARRIAGE = "\n"
+def GetHostIpList():
+    content = ""
+    with open(hostIpPath) as f:
+        content = f.read()
+    return content.split(CARRIAGE)
+
+class BoardCmd(Enum):
+    PING = 0
+    VERSION = 1
+
+
+cmdList = []
+def StubBoardResponse(hostip, cmd : BoardCmd):
+    hostIpList = GetHostIpList()
+    for ip in hostIpList:
+        if ip == hostip:
+            if cmd == BoardCmd.PING:
+                return "Reply from 172.26.96.1: bytes=32 time<1ms TTL=128"
+            elif cmd == BoardCmd.VERSION:
+                return "V300R024C00"
+            else:
+                return ""
+    print("no such hostip")
